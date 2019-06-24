a6a3cceea049cc051a13587379298724b00e61d9
wwalpha <wwalpha@gmail.com>
2019-06-24 22:30:38 +0900

    update function

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -9,7 +9,7 @@ locals {
   project_name_uc = "${local.remote_init.project_name_uc}"
   region          = "${local.remote_init.region}"
   environment     = "${local.remote_init.environment}"
-
+  account_id      = "${data.aws_caller_identity.current.account_id}"
   # -----------------------------------------------
   # Lambda Environments
   # -----------------------------------------------
@@ -21,3 +21,5 @@ locals {
   lodash = "${local.remote_layers.layers.lodash}"
   axios  = "${local.remote_layers.layers.axios}"
 }
+
+data "aws_caller_identity" "current" {}
--- terraform/m001.tf
+++ terraform/m001.tf
@@ -1,22 +1,138 @@
-# module "slack" {
-#   source = "github.com/wwalpha/terraform-modules-lambda"
+// -----------------------------------------
+// AWS SNS Topic
+// -----------------------------------------
+resource "aws_sns_topic" "m001" {
+  name                                     = "${local.project_name_uc}_LambdaError"
+  application_success_feedback_sample_rate = 0
+  http_success_feedback_sample_rate        = 0
+  sqs_success_feedback_sample_rate         = 0
 
-#   file_name = "build/index.zip"
+  policy = "${data.aws_iam_policy_document.m001_sns.json}"
+}
 
-#   function_name = "${local.project_name_uc}-M001"
-#   handler       = "index.handler"
-#   runtime       = "nodejs10.x"
-#   role_name     = "${local.project_name_uc}-M001"
-#   # role_policy_json = ["${data.aws_iam_policy_document.dynamodb_access_policy.json}"]
 
-#   timeout = 5
-# }
+// -----------------------------------------
+// AWS SNS Topic Policy
+// -----------------------------------------
+data "aws_iam_policy_document" "m001_sns" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
 
-# // -----------------------------------------
-# // Lambda Module File
-# // -----------------------------------------
-# data "archive_file" "module" {
-#   type        = "zip"
-#   source_file = "${path.module}/build/index.js"
-#   output_path = "${path.module}/build/index.zip"
-# }
+    effect = "Allow"
+
+    actions = [
+      "SNS:GetTopicAttributes",
+      "SNS:SetTopicAttributes",
+      "SNS:AddPermission",
+      "SNS:RemovePermission",
+      "SNS:DeleteTopic",
+      "SNS:Subscribe",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:Publish",
+      "SNS:Receive",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+
+      values = [
+        "${local.account_id}",
+      ]
+    }
+
+    resources = [
+      "arn:aws:sns:${local.region}:${local.account_id}:*",
+    ]
+  }
+}
+
+// -----------------------------------------
+// AWS Lambda Function
+// -----------------------------------------
+module "m001" {
+  source = "github.com/wwalpha/terraform-modules-lambda"
+
+  filename         = "${data.archive_file.m001.output_path}"
+  source_code_hash = "${filebase64sha256("${data.archive_file.m001.output_path}")}"
+
+  function_name      = "${local.project_name_uc}-M001"
+  handler            = "index.handler"
+  runtime            = "nodejs10.x"
+  memory_size        = 512
+  timeout            = 120
+  role_name          = "${local.project_name_uc}-M001Role"
+  role_policy_json   = ["${data.aws_iam_policy_document.m001_lambda.json}"]
+  trigger_principal  = "sns.amazonaws.com"
+  trigger_source_arn = "${aws_sns_topic.m001.arn}"
+  layers             = ["${local.xray}", "${local.moment}"]
+
+  variables = {
+    CALL_SLACK_FUNCTION = "PocketCards-M003"
+    GROUPNAME_PREFIX    = "/aws/lambda/PocketCards"
+  }
+}
+
+# ------------------------------
+# AWS Role Policy
+# ------------------------------
+data "aws_iam_policy_document" "m001_lambda" {
+  statement {
+    actions = [
+      "lambda:InvokeFunction",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "*",
+    ]
+  }
+}
+
+// -----------------------------------------
+// Lambda Function Module
+// -----------------------------------------
+data "archive_file" "m001" {
+  type        = "zip"
+  source_file = "../build/m001/index.js"
+  output_path = "../build/m001/index.zip"
+}
+
+// -----------------------------------------
+// AWS CloudWatch Metric Alarm
+// -----------------------------------------
+resource "aws_cloudwatch_metric_alarm" "m001" {
+  depends_on = ["aws_sns_topic.m001"]
+
+  alarm_name          = "${local.project_name_uc}-LambdaErrors"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "1"
+  alarm_description   = "This metric monitors ec2 cpu utilization"
+  datapoints_to_alarm = 1
+
+  alarm_actions = [
+    "${aws_sns_topic.m001.arn}",
+  ]
+  insufficient_data_actions = []
+  ok_actions                = []
+}
+
+// -----------------------------------------
+// AWS SNS Topic SubScription - Lambda
+// -----------------------------------------
+resource "aws_sns_topic_subscription" "m001" {
+  depends_on = ["aws_sns_topic.m001"]
+
+  topic_arn = "${aws_sns_topic.m001.arn}"
+  protocol  = "lambda"
+  endpoint  = "${module.m001.arn}"
+}
