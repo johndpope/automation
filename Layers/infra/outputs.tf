08556b3e6a0701d2824114e57caa25c79d4045c8
wwalpha <wwalpha@gmail.com>
2019-07-30 22:56:53 +0900

    remove layer to terraform repo

--- Layers/infra/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "layers" {
-  value = {
-    xray   = "${aws_lambda_layer_version.xray.arn}"
-    moment = "${aws_lambda_layer_version.moment.arn}"
-    lodash = "${aws_lambda_layer_version.lodash.arn}"
-    axios  = "${aws_lambda_layer_version.axios.arn}"
-  }
-}
